8226d4fd08cd9ac0eb34cc6800958932ef45b9ca
sean-dooher <sean@dooher.net>
2017-10-06 00:03:10 -0700

    Began making some changes, doing basic tests of data transfer

--- sentinel/hub/models.py
+++ sentinel/hub/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Leaf(models.Model):
+	name=models.CharField(maxlength=100)
+	model=models.CharField(maxlength=100)
+	serial=models.IntegerField(primary_key=True)
+	isConnected=models.BooleanField()
+
+	def list_sensors(self):
+		pass
+
+	def get_name(self):
+		pass
+
+	def set_name(self):
+		pass
+
+	def get_option(self):
+		pass
+
+	def set_option(self):
+		pass
+
+	def get_sensor(self):
+		pass
