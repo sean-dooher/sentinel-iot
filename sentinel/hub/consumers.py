cf5ab75a35e3b11cd39fa5d8601c6530030da78c
sean-dooher <sean@dooher.net>
2017-11-11 03:17:18 -0800

    Added initial leaf models, consumers for status and config, fake Javascript leaf

--- sentinel/hub/consumers.py
+++ sentinel/hub/consumers.py
@@ -1,28 +1,80 @@
 from channels import Group
+from channels.sessions import channel_session
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Leaf
+from utils import *
 import json
 # Connected to websocket.connect
 @channel_session
 def ws_add(message, group):
-    # Accept the connection
-    message.reply_channel.send({"accept": True})
-    message.channel_session['group'] = group
-    Group(group).add(message.reply_channel)
+	# Accept the connection
+	message.reply_channel.send({"accept": True})
+	message.channel_session['group'] = group
+	Group(group).add(message.reply_channel)
 
 @channel_session
 def ws_message(message):
-    # Group("chat").send({
-    #     "text": message.content['text'],
-    # })
-    # print(message.content['text'])
-    mess = json.loads(message.content['text'])
-    if('type' in mess and mess['type'] == 'RESPONSE'):
-    	print(mess['data'])
-    elif('type' in mess and mess['type'] == 'CONFIG'):
-    	message.channel_session['interface'] = mess['interface']
-    	message.channel_session['model'] = mess['model']
-    	message.channel_session['serial'] = message['serial']
-    	message.channel_session['name'] = message['name']
+	try:
+		mess = json.loads(message.content['text'])
+		if 'type' in mess:
+			type = mess['type']
+			if type == 'CONFIG':
+			   return ws_handle_config(message, mess)
+			elif type == 'DEVICE_STATUS':
+				return ws_handle_status(message, mess)
+	except json.decoder.JSONDecodeError:
+		print("invalid message")
+
+def ws_handle_config(message, mess):
+	mess = json.loads(message.content['text'])
+
+	if not is_valid_message(mess, 'CONFIG'):
+		reply = {"text":json.dumps({"type":"INVALID_CONFIG"})}
+		message.reply_channel.send(reply)
+		return
+
+	model = mess['model']
+	uuid = mess['uuid']
+	name = mess['name']
+	api = mess['api_version']
+
+	try:
+		leaf = Leaf.objects.get(pk=uuid)
+		leaf.api_version = api
+	except ObjectDoesNotExist:
+		leaf = Leaf(name=name, api_version=api, uuid=uuid, model=model)
+	leaf.save()
+	leaf.refresh_devices()
+	Group(uuid).add(message.reply_channel)
+	message.channel_session['leaf'] = uuid
+	response = {"text":json.dumps({"type":"CONFIG_COMPLETE", "hub_id":1, "uuid":uuid})}
+	message.reply_channel.send(response)
+	print('config recieved')
+
+def ws_handle_status(message, mess):
+	if not is_valid_message(mess, 'DEVICE'):
+		reply = {"text":json.dumps({"type":"INVALID_MESSAGE", "message": mess})}
+		message.reply_channel.send(reply)
+		return
+
+	if 'leaf' not in message.channel_session:
+		return
+
+	leaf = Leaf.objects.get(pk=message.channel_session['leaf'])
+	device_name = mess["device"]
+	device_format = mess["format"]
+	try:
+		device = leaf.device_set.get(name=device_name)
+	except ObjectDoesNotExist:
+		device = leaf.create_device(device_name, device_format)
+	device.update_value(mess)
+	device.save()
+	print('status updated: {}'.format(device))
 
 @channel_session
 def ws_disconnect(message):
-    Group(message.channel_session['group']).discard(message.reply_channel)
+	if 'leaf' in message.channel_session:
+		leaf = Leaf.objects.get(pk=message.channel_session['leaf'])
+		leaf.isConnected = False
+		Group(leaf.uuid).discard(message.reply_channel)
+	Group(message.channel_session['group']).discard(message.reply_channel)
--- sentinel/hub/models.py
+++ sentinel/hub/models.py
@@ -1,25 +1,167 @@
 from django.db import models
+from channels import Group
+import json
 
 class Leaf(models.Model):
-	name=models.CharField(maxlength=100)
-	model=models.CharField(maxlength=100)
-	serial=models.IntegerField(primary_key=True)
-	isConnected=models.BooleanField()
+	hub_id = 1
+	name=models.CharField(max_length=100)
+	model=models.CharField(max_length=100)
+	uuid=models.CharField(primary_key=True, max_length=36)
+	api_version=models.CharField(max_length=10)
+	isConnected=models.BooleanField(default=True)
 
-	def list_sensors(self):
-		pass
+	def set_name(self, name):
+		message = self.message_template
+		message["type"] = "SET_NAME"
+		message["name"] = name
+		self.send_message(message)
+
+	def set_option(self, device, option, value):
+		message = self.message_template
+		message["type"] = "SET_OPTION"
+		message["device"] = device
+		message["option"] = option
+		message["value"] = value
+		self.send_message(message)
+
+	def set_output(self, device, value):
+		message = self.message_template
+		message["type"] = "SET_OUTPUT"
+		message["device"] = device
+		message["value"] = value
+		self.send_message(message)
+
+	def get_option(self, device, option, update=True):
+		if update:
+			self.refresh_option(device, option)
+		#TODO: Replace following code with real code, add option
+		return self.device_set.get(name=device).option_set.filter(name=option)
+
+	def get_options(self, device, update=True):
+		if update:
+			self.refresh_options()
+		return self.device_set.get(name=device).option_set.all()
+
+	def get_device(self, device, update=True):
+		if update:
+			self.refresh_device(device)
+		return self.device_set.get(name=device)
+
+	def get_devices(self, update=True):
+		if update:
+			self.refresh_devices()
+		return self.device_set.all()
 
 	def get_name(self):
-		pass
+		return self.name
 
-	def set_name(self):
-		pass
+	def refresh_devices(self):
+		message = self.message_template
+		message["type"] = "LIST_DEVICES"
+		self.send_message(message)
 
-	def get_option(self):
-		pass
+	def refresh_name(self):
+		message = self.message_template
+		message["type"] = "GET_NAME"
+		self.send_message(message)
 
-	def set_option(self):
-		pass
+	def refresh_options(self):
+		message = self.message_template
+		message["type"] = "LIST_OPTIONS"
+		self.send_message(message)
 
-	def get_sensor(self):
-		pass
+	def refresh_device(self, device):
+		message = self.message_template
+		message["type"] = "GET_DEVICE"
+		message["device"] = device
+		self.send_message(message)
+
+	def refresh_option(self, device, option):
+		message = self.message_template
+		message["type"] = "GET_OPTION"
+		message["option"] = option
+		message["device"] = device
+		self.send_message(message)
+
+	def send_message(self, message, callback=lambda message: None):
+		message = {"text":json.dumps(message)}
+		Group(self.uuid).send(message)
+	
+	def create_device(self, device_name, format):
+		if format == 'bool':
+			device = BooleanDevice(name=device_name, leaf=self, value=False)
+		elif format == 'number+units':
+			device = UnitDevice(name=device_name, leaf=self, value=0, units="None")
+		elif format == 'number':
+			device = NumberDevice(name=device_name, leaf=self, value=0)
+		else:
+			#treat unknown formats as strings per API
+			devive = StringDevice(name=device_name, leaf=self, value="")
+		return device
+
+	@property
+	def message_template(self):
+		return {"uuid":self.uuid, "hub_id":self.hub_id}
+
+	def __repr__(self):
+		return "SentinelLeaf <name: {}, uuid:{}>".format(self.name, self.uuid)
+
+class Device(models.Model):
+	name=models.CharField(max_length=100)
+	leaf=models.ForeignKey(Leaf, on_delete=models.CASCADE)
+
+	def update_value(self, message):
+		self.value = message['value']
+
+	def __str__(self):
+		return repr(self)
+	
+	def __repr__(self):
+		return "Device <name: {}>".format(self.name)
+
+class StringDevice(Device):
+	value=models.CharField(max_length=250)
+
+	@property
+	def format(self):
+		return "string"
+
+	def __repr__(self):
+		return "StringDevice <name:{}, value: {}>".format(self.name, self.value)
+
+class BooleanDevice(Device):
+	value=models.BooleanField()
+
+	@property
+	def format(self):
+		return "bool"
+
+	def __repr__(self):
+		return "BooleanDevice <name:{}, value: {}>".format(self.name, self.value)
+
+class NumberDevice(Device):
+	value=models.DecimalField(max_digits=15, decimal_places=4)
+
+	@property
+	def format(self):
+		return "number"
+
+	def __repr__(self):
+		return "NumberDevice <name:{}, value: {}>".format(self.name, self.value)
+
+class UnitDevice(Device):
+	value=models.DecimalField(max_digits=15, decimal_places=4)
+	units=models.CharField(max_length=10)
+
+	def update_value(self, message):
+		self.units = message["units"]
+		super().update_value(message)
+
+	@property
+	def format(self):
+		return "number+units"
+
+	def __repr__(self):
+		return "UnitDevice <name:{}, value: {}>".format(self.name, self.value)
+
+
