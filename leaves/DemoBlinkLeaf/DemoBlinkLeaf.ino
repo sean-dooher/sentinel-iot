ef685c3c163b8cbf76cdd72f13daf16478797bc6
Arjun Vasudevan <arjunvasudevan@Arjuns-MacBook-Pro-2.local>
2017-11-12 16:25:09 -0800

    updated Arduino code to read binary id from id card

--- leaves/DemoBlinkLeaf/DemoBlinkLeaf.ino
+++ leaves/DemoBlinkLeaf/DemoBlinkLeaf.ino
@@ -1,3 +1,4 @@
+
 /*
  * WebSocketClient.ino
  *
@@ -6,52 +7,72 @@
  */
 
 #include <Arduino.h>
+#include <ArduinoJson.h>
+
+
 
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <ArduinoOTA.h>
 #include <WebSocketsClient.h>
-#include <Wiegand.h>
+#include <WiegandNG.h>
 
-WIEGAND wg;
+
 ESP8266WiFiMulti WiFiMulti;
 WebSocketsClient webSocket;
 
 #define USE_SERIAL Serial1
+WiegandNG rfidReader;
+ 
+long facilityCode;
+long cardCode;
 
-void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+void webSocketEvent(WStype_t type, uint8_t* payload, size_t length) {
 
   switch(type) {
     case WStype_DISCONNECTED:
       USE_SERIAL.printf("[WSc] Disconnected!\n");
       break;
     case WStype_CONNECTED: {
+      const size_t bufferSize = JSON_OBJECT_SIZE(5);
+      DynamicJsonBuffer jsonBuffer(bufferSize);
+
+      JsonObject& root = jsonBuffer.createObject();
+      root["type"] = "CONFIG";
+      root["uuid"] = "0d51974f-90f1-4fec-af45-5bf319186304";
+      root["name"] = "RFID_READER";
+      root["model"] = "THINLINE_II";
+      root["api_version"] = "0.1.0";
+
+      char buffer[512];
+      root.printTo(buffer, sizeof(buffer));
+      
+      
       USE_SERIAL.printf("[WSc] Connected to url: %s\n", payload);
+      
 
       // send message to server when Connected
-      webSocket.sendTXT("Connected");
+      webSocket.sendTXT(buffer);
     }
       break;
-    case WStype_TEXT:
-      USE_SERIAL.printf("[WSc] get text: %s\n", payload);
-      char* buff;
-      buff = (char*) malloc(4*sizeof(char));
-      int i;
-      for(i = 0; i < 3 && *payload != '\0'; i++) {
-        buff[i] = (char) *(payload++);
+    case WStype_TEXT: {
+
+      // Case: GET_DEVICE websocket event
+      String payloadString = String((char*) payload);
+      const size_t bufferSize = JSON_OBJECT_SIZE(4) + 210;
+      DynamicJsonBuffer jsonBuffer(bufferSize);      
+      JsonObject& root = jsonBuffer.parseObject(payloadString);
+      
+      const char* type = root["type"]; // "GET_DEVICE
+      const char* uuid = root["uuid"]; // "0d51974f-90f1-4fec-af45-5bf319186304"
+      const char* hub_id = root["hub_id"]; // "666"
+      
+      const char* name = root["name"]; // "RFID_THINLINE_II"
+
+      if (strcmp(type, "GET_DEVICE") == 0) {
+        sendDeviceStatus();   
       }
-      buff[i] = '\0';
-      if(strcmp(buff,"on") == 0) {
-        digitalWrite(4, HIGH);
-      } else if(strcmp(buff,"off") == 0) {
-        digitalWrite(4, LOW);
-      } else {
-//         digitalWrite(4, HIGH);
-//         delay(1000);                       // wait for a second
-//         digitalWrite(4, LOW);    // turn the LED off by making the voltage LOW
-//         delay(1000);      
       }
-      free(buff);
       break;
     case WStype_BIN:
       USE_SERIAL.printf("[WSc] get binary length: %u\n", length);
@@ -65,8 +86,8 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
 }
 
 void setup() {
-  wg.begin(5, digitalPinToInterrupt(5), 4, digitalPinToInterrupt(4));
-  WiFiMulti.addAP("The Loft", "icysocks019");
+  rfidReader.begin(5,digitalPinToInterrupt(5), 4, digitalPinToInterrupt(4), 100, 25);
+  WiFiMulti.addAP("The Loft", "");
 
   //WiFi.disconnect();
   while(WiFiMulti.run() != WL_CONNECTED) {
@@ -97,7 +118,7 @@ void setup() {
   ArduinoOTA.setHostname("RFID-Node");
 
   // server address, port and URL
-  webSocket.begin("192.168.1.8", 8000, "/");
+  webSocket.begin("192.168.1.6", 8000, "/hub/");
 
   // event handler
   webSocket.onEvent(webSocketEvent);
@@ -110,14 +131,63 @@ void setup() {
 
 }
 
+void processReaderData(long* facilityCode, long* cardCode) {
+  *facilityCode = 0;
+  *cardCode = 0;
+  volatile unsigned char *buffer=rfidReader.getRawData();
+  unsigned int bufferSize = rfidReader.getBufferSize();
+  unsigned int countedBits = rfidReader.getBitCounted();
+ 
+  unsigned int countedBytes = (countedBits/8);
+  if ((countedBits % 8)>0) countedBytes++;
+  unsigned int bitsUsed = countedBytes * 8;
+  int index = 0;
+  for (int i=bufferSize-countedBytes; i< bufferSize;i++) {
+    unsigned char bufByte=buffer[i];
+    for(int x=0; x<8;x++) {
+      if ( (((bufferSize-i) *8)-x) <= countedBits) {
+        if(index > 1 && index < 14) {
+          *facilityCode <<=1;
+          if((bufByte & 0x80)) {
+            *facilityCode |= 1;
+          }
+        } else if (index >= 14 && index < 34) {
+          *cardCode <<=1;
+          if((bufByte & 0x80)) {
+            *cardCode |= 1;
+          }
+        }
+        index++;
+      }
+      bufByte<<=1;
+    }
+  }
+}
+
+void sendDeviceStatus() {
+  const size_t bufferSize = JSON_OBJECT_SIZE(5);
+  DynamicJsonBuffer jsonBuffer(bufferSize);
+
+  JsonObject& root = jsonBuffer.createObject();
+  root["type"] = "DEVICE_STATUS";
+  root["uuid"] = "0d51974f-90f1-4fec-af45-5bf319186304";
+  root["device"] = "rfidReader";
+  root["value"] = cardCode;
+  root["format"] = "number";
+
+  char buffer[512];
+  root.printTo(buffer, sizeof(buffer));
+
+  webSocket.sendTXT(buffer);  
+}
+
 void loop() {
   ArduinoOTA.handle();
-  if(wg.available())
+  if(rfidReader.available())
   {
-    char* cardCodeString = (char*) malloc(100*sizeof(char));
-      sprintf(cardCodeString, "%ld", wg.getCode());
-      webSocket.sendTXT(cardCodeString);
-      free(cardCodeString);
+    processReaderData(&facilityCode, &cardCode); //store data in facilityCode and cardCode global variables
+    rfidReader.clear();
+    sendDeviceStatus();
   }
 
   webSocket.loop();
