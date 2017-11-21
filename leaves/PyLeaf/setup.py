3f0de7df03e44deeff2c30e2160bad265fd4e524
sean-dooher <sean@dooher.net>
2017-11-21 04:09:00 -0800

    Added setup for PyLeaf module

---
+++ leaves/PyLeaf/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyLeaf',
+      version='0.1',
+      description='Sentinel Leaf Implementation in Python',
+      author='Sean Dooher',
+      author_email='sean@dooher.net',
+      url='https://github.com/sean-dooher/sentinel-iot',
+      py_modules=['PyLeaf'],
+     )
