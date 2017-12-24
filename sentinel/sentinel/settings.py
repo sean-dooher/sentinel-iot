01cf7868b7318cd2a390ffc827b35d12294f8057
sean <sean@dooher.net>
2017-12-24 01:41:55 -0800

    Added logging

--- sentinel/sentinel/settings.py
+++ sentinel/sentinel/settings.py
@@ -107,6 +107,21 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+        }
+    },
+    'loggers': {
+        'hub.consumers': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        }
+    }
+}
 
 
 # Internationalization
