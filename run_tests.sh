beed04299a3d26c30be196df443e7380766dae9f
Sean Dooher <sean@dooher.net>
2018-07-21 01:37:26 -0700

    Add noinput to test runner

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,4 @@ dockerize -wait tcp://database:5432
 dockerize -wait tcp://redis:6379
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py jenkins --enable-coverage
+python3 manage.py jenkins --enable-coverage --noinput
