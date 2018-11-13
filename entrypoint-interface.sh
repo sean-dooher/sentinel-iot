d28b9714c3643610f57b9253a8ee9f718a9a7514
Sean Dooher <sean@dooher.net>
2018-11-13 04:29:35 -0800

    Fix channels 2.0 misconfiguration

--- entrypoint-interface.sh
+++ entrypoint-interface.sh
@@ -4,4 +4,4 @@ dockerize -wait tcp://redis:6379
 
 python manage.py migrate
 python manage.py collectstatic --noinput
-exec daphne -b 0.0.0.0 -p 8000 ${PROJECT_NAME}.asgi:channel_layer
+exec daphne -b 0.0.0.0 -p 8000 ${PROJECT_NAME}.asgi:application
--- sentinel/sentinel/asgi.py
+++ sentinel/sentinel/asgi.py
@@ -1,6 +1,7 @@
 import os
-from channels.asgi import get_channel_layer
+import django
+from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sentinel.settings")
-
-channel_layer = get_channel_layer()
+django.setup()
+application = get_default_application()
